67acc44819dcde894408699d1ed39fa538cb9999
Joe MacLennan <maclennan.joe@gmail.com>
2022-06-27 22:23:41 -0400

    add nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import archiveAlt from '../assets/img/archive-alt.svg';
 import archive from '../assets/img/archive.svg';
 import barChartAlt from '../assets/img/bar-chart-alt.svg';
@@ -20,98 +20,50 @@ const NavBar: FC = () => {
       route: '/collections',
       imgSrc: layers,
       imgSrcAlt: layersAlt
+    },
+    {
+      title: 'Inventory',
+      route: '/inventory',
+      imgSrc: shoppingBag,
+      imgSrcAlt: shoppingBagAlt
+    },
+    {
+      title: 'Archive',
+      route: '/archive',
+      imgSrc: archive,
+      imgSrcAlt: archiveAlt
+    },
+    {
+      title: 'Analytics',
+      route: '/analytics',
+      imgSrc: barChart,
+      imgSrcAlt: barChartAlt
+    },
+    {
+      title: 'Settings',
+      route: '/settings',
+      imgSrc: settings,
+      imgSrcAlt: settingsAlt
     }
   ];
+
+  const [selectedRoute, setSelectedRoute] = useState<string>('Collections');
 
   return (
     <div className='flex flex-col bg-secondary w-64 h-screen space-y-6'>
       <img src={logo} alt='' className='max-w-[80%]' />
-      <ul className='flex flex-col space-y-2 ml-4 w-52'>
+      <nav className='flex flex-col space-y-2 items-center'>
         {navBarItems.map((i) => (
           <NavBarListItem
             imgSrc={i.imgSrc}
             imgSrcAlt={i.imgSrcAlt}
             title={i.title}
             route={i.route}
+            onClick={() => setSelectedRoute(i.title)}
+            selected={selectedRoute === i.title}
           />
         ))}
-        <li className='group hover:bg-primary rounded'>
-          <a href='/inventory' className='flex flex-row items-center'>
-            <div className='my-1'>
-              <img
-                src={shoppingBag}
-                alt=''
-                className='group-hover:hidden h-[24px] ml-2'
-              />
-              <img
-                src={shoppingBagAlt}
-                alt=''
-                className='hidden h-[24px] ml-2 group-hover:block'
-              />
-            </div>
-            <div className='ml-4 text-primary text-sm font-semibold group-hover:text-white'>
-              Inventory
-            </div>
-          </a>
-        </li>
-        <li className='group hover:bg-primary rounded'>
-          <a href='/archive' className='flex flex-row items-center'>
-            <div className='my-1'>
-              <img
-                src={archive}
-                alt=''
-                className='group-hover:hidden h-[24px] ml-2'
-              />
-              <img
-                src={archiveAlt}
-                alt=''
-                className='hidden h-[24px] ml-2 group-hover:block'
-              />
-            </div>
-            <div className='ml-4 text-primary text-sm font-semibold group-hover:text-white'>
-              Archive
-            </div>
-          </a>
-        </li>
-        <li className='group hover:bg-primary rounded'>
-          <a href='/analytics' className='flex flex-row items-center'>
-            <div className='my-1'>
-              <img
-                src={barChart}
-                alt=''
-                className='group-hover:hidden h-[24px] ml-2'
-              />
-              <img
-                src={barChartAlt}
-                alt=''
-                className='hidden h-[24px] ml-2 group-hover:block'
-              />
-            </div>
-            <div className='ml-4 text-primary text-sm font-semibold group-hover:text-white'>
-              Analytics
-            </div>
-          </a>
-        </li>
-        <li className='group hover:bg-primary rounded'>
-          <a href='/settings' className='flex flex-row items-center'>
-            <div className='my-0.5'>
-              <img
-                src={settings}
-                alt=''
-                className='group-hover:hidden h-[30px] ml-1'
-              />
-              <img
-                src={settingsAlt}
-                alt=''
-                className='hidden h-[30px] ml-1 group-hover:block'
-              />
-            </div>
-            <div className='ml-3 text-primary text-sm font-semibold group-hover:text-white'>
-              Settings
-            </div>
-          </a>
-        </li>
-      </ul>
+      </nav>
     </div>
   );
 };
--- src/components/NavBarListItem.tsx
+++ src/components/NavBarListItem.tsx
@@ -1,27 +1,48 @@
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
+import { Link } from 'react-router-dom';
 import { NavBarItem } from '../types/NavBarItem';
 
-const NavBarListItem: FC<NavBarItem> = (props) => {
+interface NavBarListItemProps extends NavBarItem {
+  onClick?: MouseEventHandler<HTMLElement>;
+  selected?: boolean;
+}
+
+const NavBarListItem: FC<NavBarListItemProps> = (props) => {
   return (
-    <li className='group hover:bg-primary rounded'>
-      <a href={props.route} className='flex flex-row items-center'>
-        <div className='my-1'>
-          <img
-            src={props.imgSrc}
-            alt=''
-            className='group-hover:hidden h-[24px] ml-2'
-          />
-          <img
-            src={props.imgSrcAlt}
-            alt=''
-            className='hidden h-[24px] ml-2 group-hover:block'
-          />
-        </div>
-        <div className='ml-4 text-primary text-sm font-semibold group-hover:text-white'>
-          {props.title}
-        </div>
-      </a>
-    </li>
+    <Link
+      key={props.title}
+      to={props.route}
+      onClick={props.onClick}
+      className={
+        'group flex flex-row items-center w-5/6 rounded hover:bg-primary' +
+        (props.selected ? ' bg-primary' : '')
+      }>
+      <div className='my-1'>
+        <img
+          src={props.imgSrc}
+          alt=''
+          className={
+            'h-[24px] ml-2 group-hover:hidden' +
+            (props.selected ? ' hidden' : '')
+          }
+        />
+        <img
+          src={props.imgSrcAlt}
+          alt=''
+          className={
+            'h-[24px] ml-2 group-hover:block' +
+            (props.selected ? ' block' : ' hidden')
+          }
+        />
+      </div>
+      <div
+        className={
+          'ml-4 text-primary text-sm font-semibold group-hover:text-white' +
+          (props.selected ? ' text-white' : '')
+        }>
+        {props.title}
+      </div>
+    </Link>
   );
 };
 
